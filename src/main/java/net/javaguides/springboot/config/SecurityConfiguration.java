50dd8e544f91c310137f762a3c1c2805a43974f1
timursharipov <timvlpn@gmail.com>
2020-10-04 01:24:44 +0300

    first

--- src/main/java/net/javaguides/springboot/config/SecurityConfiguration.java
+++ src/main/java/net/javaguides/springboot/config/SecurityConfiguration.java
@@ -1,62 +1,49 @@
 package net.javaguides.springboot.config;
 
+import net.javaguides.springboot.security.LoginSuccessHandler;
+import net.javaguides.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import net.javaguides.springboot.service.UserService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserService userDetailsService;
+    @Autowired
+    LoginSuccessHandler loginSuccessHandler;
 
-	@Autowired
-	private UserService userService;
-	
-	@Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-	
-	@Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-        auth.setUserDetailsService(userService);
-        auth.setPasswordEncoder(passwordEncoder());
-        return auth;
-    }
-	
-	@Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(
-				 "/registration**",
-	                "/js/**",
-	                "/css/**",
-	                "/img/**").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin()
-		.loginPage("/login")
-		.permitAll()
-		.and()
-		.logout()
-		.invalidateHttpSession(true)
-		.clearAuthentication(true)
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		.logoutSuccessUrl("/login?logout")
-		.permitAll();
-	}
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+
+                .antMatchers("/user/**").access("hasAnyRole('USER')")
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .and().formLogin()
+                .successHandler(loginSuccessHandler);
+        http.logout()
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and().csrf().disable();
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 }
--- src/main/java/net/javaguides/springboot/service/UserService.java
+++ src/main/java/net/javaguides/springboot/service/UserService.java
@@ -1,10 +1,24 @@
 package net.javaguides.springboot.service;
 
+import net.javaguides.springboot.models.Role;
+import net.javaguides.springboot.models.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import net.javaguides.springboot.model.User;
-import net.javaguides.springboot.web.dto.UserRegistrationDto;
 
-public interface UserService extends UserDetailsService{
-	User save(UserRegistrationDto registrationDto);
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+    void addUser(User user);
+
+    void updateUser(User user);
+
+    void removeUserById(Long id);
+
+    User getUserById(Long id);
+
+    List<User> listUsers();
+
+    void saveRoles(List<Role> roles);
+
+    List<Role> getAllRoles();
 }
